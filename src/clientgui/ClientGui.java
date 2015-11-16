095370124d36f2b96823f91bd3872c7298d62fe3
pratyush <toppratyush007@gmail.com>
2015-11-16 15:05:13 -0500

    added threads starting processserver and listener

--- src/clientgui/ClientGui.java
+++ src/clientgui/ClientGui.java
@@ -5,10 +5,13 @@
  */
 package clientgui;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +21,14 @@ import java.util.logging.Logger;
  */
 public class ClientGui {
 
+    public static int migration =0;
+    public static int totalObjects =0;
+    
+    /**
+     *Static List containing the objects for migration
+     */
+    @SuppressWarnings("Convert2Diamond")
+    public static List<Object> objectList = new ArrayList<Object>();
     /**
      * @param args the command line arguments
      */
@@ -28,7 +39,8 @@ public class ClientGui {
             //InputStream isr=
                     
                     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                    
+                    startServer();
+                    startListener();
              while(true){
                  //System.out.print(">>");
                  //s=reader.readLine();
@@ -38,6 +50,16 @@ public class ClientGui {
         } catch (IOException ex) {
             Logger.getLogger(ClientGui.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private static void startListener() {
+        Thread listenerThread = new Thread(new JavaPMListener());
+        listenerThread.start();
+    }
+
+    private static void startServer() {
+        Thread serverThread = new Thread(new ClientGuiProcessServer());
+        serverThread.start();
     }
     
 }
